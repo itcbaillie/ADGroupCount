35ee62fd696446cb3379665bb532259d199d27cd
Chris Baillie <chris@chrisbaillie.net>
2018-07-06 12:53:26 +0100

    Initial commit

---
+++ Get-ADGroupCount.TempPoint.ps1
@@ -0,0 +1,14 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2018 v5.5.152
+	 Created on:   	06/07/2018 12:35
+	 Created by:   	c.baillie
+	 Organization: 	
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		PowerShell script to query AD Groups for membership list and count
+#>
+$Group = Read-Host -Prompt "Provide the group name: "
+Write-Host $Group
---
+++ Get-ADGroupCount.ps1
@@ -0,0 +1,52 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2018 v5.5.152
+	 Created on:   	06/07/2018 12:35
+	 Created by:   	c.baillie
+	 Organization: 	
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		PowerShell script to query AD Groups for membership list and count
+#>
+
+$Folder = "C:\temp\"
+if (-not (Test-Path $Folder))
+{
+	md $Folder	
+}
+
+Add-Type -AssemblyName PresentationFramework
+$Group = Read-Host -Prompt "Provide the group name: "
+$File = $Folder + $Group + ".txt"
+
+if (Test-Path $File)
+{
+	Remove-Item $File	
+}
+
+$Count = 0;
+try
+{
+	$Members = Get-ADGroupMember -Identity $Group
+	
+	ForEach ($Member in $Members)
+	{
+		$Count++
+		$Member.name | Out-File $File -Append
+	}
+	
+	$OutStr = "There are " + $Count + " users in this group."
+	[System.Windows.MessageBox]::Show($OutStr, $Group)
+	
+	if (Test-Path $File)
+	{
+		notepad.exe $File
+	}
+}
+catch
+{
+	$OutStr = "I couldn't find a group called " + $Group
+	[System.Windows.MessageBox]::Show($OutStr, "Error", 'OK', 'Error')	
+}
